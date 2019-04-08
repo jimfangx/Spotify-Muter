6a194ded247806c5db14da52ca2f8bc8182bb3c8
AirFusion <jfang.cv.ca.us@gmail.com>
2019-04-07 17:42:07 -0700

    update to say auth successful in main page after auth.
    Need to have a error handeler

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var request = require('request')
 const { app, BrowserWindow, ipcMain, Menu, ClientRequest, session } = electron;
 var config = require('./config.json')
 var querystring = require('querystring');
- /* Load the HTTP library */
- var http = require("http");
+/* Load the HTTP library */
+var http = require("http");
 
 let mainWindow;
 
 
 
- /* Create an HTTP server to handle responses */
+/* Create an HTTP server to handle responses */
 
- http.createServer(function(request, response) {
-   response.writeHead(200, {"Content-Type": "text/plain"});
-   response.write("Auth Successful - Please close this window.");
-   response.end();
- }).listen(8888);
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Auth Successful - Please close this window.");
+    response.end();
+}).listen(8888);
 
 app.on('ready', () => {
     console.log(`I am ready @ ${Date()}`)
@@ -37,43 +37,44 @@ app.on('ready', () => {
     //             '&redirect_uri=' + encodeURIComponent(redirect_uri));
     //     });
     // })
-    ipcMain.on("oauth", (event, getToken) => {
-        var generateRandomString = function (length) {
-            var text = '';
-            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    // ipcMain.on("oauth", (event, getToken) => {
+    var generateRandomString = function (length) {
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-            for (var i = 0; i < length; i++) {
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-            }
-            return text;
-        };
-        var state = generateRandomString(16);
+        for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    };
+    var state = generateRandomString(16);
 
-        var authWindow = new BrowserWindow({ width: 800, height: 600, show: false, 'node-integration': false });
-        var spotifyUrl = 'https://accounts.spotify.com/authorize?';
-        var authUrl = spotifyUrl + 'client_id=' + config.clientID + '&response_type=code' + `&redirect_uri=http://localhost:8888` + '&scope=' + config.scope + '&state' + state;
-        // console.log(authUrl)
-        authWindow.loadURL(authUrl);
-        authWindow.show();
-        var usrAuthCode;
-        session.defaultSession.webRequest.onCompleted((details) => {
-            // get the cookie after redirect from the page
-            var usrAuthCode = details.url.substring(details.url.indexOf("=")+1, details.url.length)
-            console.log(usrAuthCode) // need to get this out
-            
-        })
+    var authWindow = new BrowserWindow({ width: 800, height: 600, show: false, 'node-integration': false });
+    var spotifyUrl = 'https://accounts.spotify.com/authorize?';
+    var authUrl = spotifyUrl + 'client_id=' + config.clientID + '&response_type=code' + `&redirect_uri=http://localhost:8888` + '&scope=' + config.scope + '&state' + state;
+    // console.log(authUrl)
+    authWindow.loadURL(authUrl);
+    authWindow.show();
+
+    session.defaultSession.webRequest.onCompleted((details) => {
+        // get the cookie after redirect from the page
+        var userauthcode = details.url.substring(details.url.indexOf("=") + 1, details.url.length)
         
-
-        // request(authUrl, function (error, response, body) {
-        //     console.log('error:', error); // Print the error if one occurred
-        //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        //     console.log('body:', body); // Print the HTML for the Google homepage.
-        // })
-        // console.log(authUrl)
-
+        mainWindow.webContents.send('authCode:sent', userauthcode)
     })
 
+
+
+    // request(authUrl, function (error, response, body) {
+    //     console.log('error:', error); // Print the error if one occurred
+    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //     console.log('body:', body); // Print the HTML for the Google homepage.
+    // })
+    // console.log(authUrl)
+
 })
+
+// })
 
 
 // const menuTemplate = [
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var request = require('request') 
+var request = require('request')
 const electron = require('electron')
 const { ipcRenderer } = electron;
 var config = require('./config.json')
@@ -15,9 +15,30 @@ var config = require('./config.json')
 //     'web-security': false
 // });
 
-document.querySelector('#spotifyAuthSubmit').addEventListener('click', (event) => {
-    ipcRenderer.send('oauth', 'getToken')
-    console.log("sent")
+// document.querySelector('#spotifyAuthSubmit').addEventListener('click', (event) => {
+//     ipcRenderer.send('oauth', 'getToken')
+//     console.log("sent")
+// })
+ipcRenderer.on('authCode:sent', (event, code) => {
+    // document.querySelector('#spotifyAuthSubmit').parentNode.removeChild(document.getElementById("spotifyAuthSubmit"))
+    document.getElementById('success').innerHTML = "Auth successful!"
+    console.log("trading")
+    request({
+        url: "https://accounts.spotify.com/api/token",
+        body: {
+            grant_type: "authorization_code",
+            code: "code",
+            redirect_uri: "http://localhost:8888"
+            //https://medium.com/@davidjtomczyk/spotify-api-authorization-flow-with-react-and-rails-7f42845a43c
+        },
+        Headers: {
+            authorization: config.authorization
+        }, function(error, response, body) {
+            console.log('error:', error); // Print the error if one occurred
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            console.log('body:', body); // Print the HTML for the Google homepage.
+        }
+    })
 })
  // Watching for submit event in form tags
 
